40f478b424cae4b761f2effe24190934ab0a6035
Ritik Ranjan Pati <ritikranjanpati@gmail.com>
2026-01-07 21:18:51 +0530

    Create variable.tf

---
+++ terraform/variable.tf
@@ -0,0 +1,35 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+}
+
+variable "public_subnets" {
+  description = "Public subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "Private subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "db_username" {
+  description = "RDS master username"
+  type        = string
+}
+
+variable "db_password" {
+  description = "RDS master password"
+  type        = string
+  sensitive   = true
+}
+
+variable "my_ip" {
+  description = "Your public IP for SSH access"
+  type        = string
+}
